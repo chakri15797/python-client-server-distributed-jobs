d07503d94f87d014372997ca5d914a9002fb54e1
ChakradharNadikota <chakradharnadikota@NADIKOTAs-MacBook-Air.local>
2024-02-27 02:16:25 -0600

    Chnages

--- central-server.py
+++ central-server.py
@@ -3,6 +3,10 @@ import threading
 from queue import Queue
 from itertools import count
 import json
+import atexit
+import os
+import time
+import sys
 
 HOST = 'localhost'
 PORT = 5000
@@ -12,6 +16,18 @@ CLIENTS = []
 command_counter = count(1)
 client_counter = count(1)
 THREAD_STATUS = {}
+SHUTDOWN_FLAG = False  # Flag to indicate shutdown
+THREADS=[]
+
+def is_socket_closed(sock):
+    try:
+        data = sock.recv(1)
+        if not data:
+            return True
+        else:
+            return False
+    except socket.error as e:
+        return True
 
 def find_free_port(start_port):
     port = start_port
@@ -28,54 +44,71 @@ def write_server_info_to_file(port):
         file.write(f"{HOST}:{port}")
 
 def handle_client(conn, addr):
+    global COMMANDS, SHUTDOWN_FLAG, CLIENTS
     client_id = next(client_counter)
     print(f"Client {client_id} connected from {addr}")
     client_queue = Queue()
     CLIENTS.append((client_id, conn, addr, client_queue))
     THREAD_STATUS[client_id] = 'Connected'  # Update status when client connects
-    while True:
+    while not SHUTDOWN_FLAG:
         try:
             if not client_queue.empty():
+                if THREAD_STATUS[client_id] == 'Disconnected':
+                    COMMANDS.put((command["command_id"],command["command"]))
+                    break
                 command = client_queue.get()
                 print(f"Sending command {command} to client {client_id}")
                 conn.sendall(json.dumps(command).encode())
                 response = conn.recv(1024).decode()
                 print(f"Received response from client {client_id}: {response}")
             else:
-                threading.Event().wait(1)  # Wait for 1 seconds before checking again
+                if THREAD_STATUS[client_id] == 'Disconnected':
+                    break
+                time.sleep(1)  # Wait for 1 second before checking again        
         except Exception as e:
+            COMMANDS.put((command["command_id"],command["command"]))
             print(f"Error receiving data from client {client_id}: {e}")
             break
     print(f"Client {client_id} disconnected")
-    CLIENTS.remove((client_id, conn, addr, client_queue))
-    THREAD_STATUS[client_id] = 'Disconnected'  # Update status when client disconnects
+    temp = (client_id, conn, addr, client_queue)
+    if (client_id, conn, addr, client_queue) in CLIENTS:
+        CLIENTS.remove(temp)
+        THREAD_STATUS[client_id] = 'Disconnected'  # Update status when client disconnects
     conn.close()
 
-
 def process_commands():
-    while True:
-        command_id, command = COMMANDS.get()
-        if command_id == -1:
-            break
-        if CLIENTS:
-            client_id, _, _, client_queue = min(CLIENTS, key=lambda x: x[3].qsize())
-            client_queue.put({"client_id": client_id, "command_id": command_id, "command": command})
+    global SHUTDOWN_FLAG, COMMANDS, CLIENTS
+    while not SHUTDOWN_FLAG:
+        if not COMMANDS.empty() and len(CLIENTS)!=0:
+            print(list(COMMANDS.queue))
+            command_id, command = COMMANDS.get()
+            print(command)
+            if command_id == -1:
+                break
+            if CLIENTS:
+                client_id, _, _, client_queue = min(CLIENTS, key=lambda x: x[3].qsize())
+                client_queue.put({"client_id": client_id, "command_id": command_id, "command": command})
 
 def start_server():
+    global SHUTDOWN_FLAG
     port = find_free_port(PORT)
     write_server_info_to_file(port)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, port))
         server_socket.listen()
         print(f"Server listening on {HOST}:{port}")
-        threading.Thread(target=process_commands).start()
-        threading.Thread(target=ask_for_commands).start()
-        while True:
+        (t := threading.Thread(name="ask_for_commands",target=ask_for_commands)).start()
+        THREADS.append(t)
+        (t := threading.Thread(name="process_commands",target=process_commands)).start()
+        THREADS.append(t)
+        while not SHUTDOWN_FLAG:
             conn, addr = server_socket.accept()
-            threading.Thread(target=handle_client, args=(conn, addr)).start()
+            (t := threading.Thread(name="handle_client",target=handle_client, args=(conn, addr))).start()
+            THREADS.append(t)
 
 def ask_for_commands():
-    while True:
+    global SHUTDOWN_FLAG, COMMANDS
+    while not SHUTDOWN_FLAG:
         command = input("Enter command to send to clients (or 'exit' to quit): ")
         if not command:  # Check if the command is empty
             continue
@@ -86,14 +119,44 @@ def ask_for_commands():
             COMMANDS.put((next(command_counter), command))
 
 def monitor_threads():
-    with open('thread_status.txt', 'w') as file:
-        while True:
+    global SHUTDOWN_FLAG
+    with open('logs/thread_status.txt', 'w') as file:
+        while not SHUTDOWN_FLAG:  # Continue monitoring until shutdown flag is set
             for client_id, status in THREAD_STATUS.items():
                 file.write(f"Client {client_id}: {status}\n")
             file.write("\n")
             file.flush()
-            threading.Event().wait(60)  # Wait for 5 seconds before checking again
+            time.sleep(10)  # Wait for 10 seconds before checking again
+
+def monitor_clients():
+    global SHUTDOWN_FLAG
+    while not SHUTDOWN_FLAG:  # Continue monitoring until shutdown flag is set
+        for client_id, conn, addr, client_queue in CLIENTS:
+            if is_socket_closed(conn):
+                THREAD_STATUS[client_id] = 'Disconnected'
+                if not SHUTDOWN_FLAG:
+                    CLIENTS.remove((client_id, conn, addr, client_queue))
+        time.sleep(0.1)
+
+
+def exit_handler():
+    print("Exiting...")
 
 if __name__ == "__main__":
-    threading.Thread(target=monitor_threads).start()
-    start_server()
+    try:
+        directory = 'logs'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        (t := threading.Thread(name="monitor_threads",target=monitor_threads)).start()
+        THREADS.append(t)
+        (t := threading.Thread(name="monitor_clients",target=monitor_clients)).start()
+        THREADS.append(t)
+        start_server()
+    except KeyboardInterrupt:
+        SHUTDOWN_FLAG = True  # Set the shutdown flag when Ctrl+C is pressed     
+        print("Shutting down server...")
+        for t in THREADS:
+            if t.is_alive():
+                t.join(timeout=1)
+    finally:
+        atexit.register(exit_handler)
--- client.py
+++ client.py
@@ -1,8 +1,26 @@
 import socket
 import subprocess
-import json
+import json 
+import os
 
 FILENAME = 'server_info.txt'
+CLIENT_ID = None
+BUFFER = ""
+LOG_FILE= None
+
+
+def print_and_log(message):
+    global CLIENT_ID, BUFFER, LOG_FILE
+    if CLIENT_ID is not None:
+        if not LOG_FILE:
+            LOG_FILE = open(f"logs/client_{CLIENT_ID}.log", 'w')
+        if BUFFER != "":
+            message = BUFFER + message
+            BUFFER = ""
+        LOG_FILE.write(message + '\n')
+    else:
+        BUFFER += message + "\n"
+    print(message)
 
 def read_server_info_from_file():
     with open(FILENAME, 'r') as file:
@@ -10,29 +28,42 @@ def read_server_info_from_file():
     return server_info[0], int(server_info[1])
 
 def connect_to_server():
+    global CLIENT_ID, LOG_FILE
     server_host, server_port = read_server_info_from_file()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((server_host, server_port))
-        print(f"Connected to server {server_host}:{server_port}")
+        print_and_log(f"Connected to server {server_host}:{server_port}")
         while True:
             try:
                 command = client_socket.recv(1024).decode()
                 if not command:
-                    print("Empty command received")
+                    print_and_log("Empty command received")
                     break
-                print(f"Received command: {command}")
+                print_and_log(f"Received command: {command}")
                 command_data = json.loads(command)
                 client_id = command_data["client_id"]
+                CLIENT_ID = client_id
                 command_id = command_data["command_id"]
-                command_text = command_data["command"]
+                command_text = command_data["command"].strip()
+                if command_text=='ping':
+                    continue
                 output = subprocess.check_output(command_text, shell=True).decode()
-                print(f"Processed command: {command_text}")
-                print(f"Output: {output}")
+                print_and_log(f"Processed command: {command_text}")
+                print_and_log(f"Output: {output}")
                 response = {"client_id": client_id, "command_id": command_id, "response": output}
                 client_socket.sendall(json.dumps(response).encode())
+                LOG_FILE.flush
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt: Stopping the client")
+                client_socket.close()
+                break
             except Exception as e:
-                print(f"Error processing command: {e}")
+                print_and_log(f"Error processing command: {e}")
+                client_socket.close()
                 break
 
 if __name__ == "__main__":
+    directory = 'logs'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     connect_to_server()
