08f15e67c5bcb4d10d47b4020f1bcb1beb0e82fd
ChakradharNadikota <chakradharnadikota@NADIKOTAs-MacBook-Air.local>
2024-02-27 16:20:51 -0600

    Thread exit issues resolved

--- central-server.py
+++ central-server.py
@@ -7,6 +7,8 @@ import atexit
 import os
 import time
 import sys
+import queue
+from signal import pthread_kill, SIGUSR1, SIGTSTP
 
 HOST = 'localhost'
 PORT = 5000
@@ -17,7 +19,7 @@ command_counter = count(1)
 client_counter = count(1)
 THREAD_STATUS = {}
 SHUTDOWN_FLAG = False  # Flag to indicate shutdown
-THREADS=[]
+THREADS = []
 
 def is_socket_closed(sock):
     try:
@@ -64,7 +66,7 @@ def handle_client(conn, addr):
             else:
                 if THREAD_STATUS[client_id] == 'Disconnected':
                     break
-                time.sleep(1)  # Wait for 1 second before checking again        
+                time.sleep(1)  # Wait for 1 second before checking again
         except Exception as e:
             COMMANDS.put((command["command_id"],command["command"]))
             print(f"Error receiving data from client {client_id}: {e}")
@@ -76,18 +78,18 @@ def handle_client(conn, addr):
         THREAD_STATUS[client_id] = 'Disconnected'  # Update status when client disconnects
     conn.close()
 
+
 def process_commands():
     global SHUTDOWN_FLAG, COMMANDS, CLIENTS
     while not SHUTDOWN_FLAG:
         if not COMMANDS.empty() and len(CLIENTS)!=0:
-            print(list(COMMANDS.queue))
-            command_id, command = COMMANDS.get()
-            print(command)
+            command_id, command = COMMANDS.get(timeout=1)
             if command_id == -1:
                 break
             if CLIENTS:
                 client_id, _, _, client_queue = min(CLIENTS, key=lambda x: x[3].qsize())
                 client_queue.put({"client_id": client_id, "command_id": command_id, "command": command})
+    print("Exiting process_commands")
 
 def start_server():
     global SHUTDOWN_FLAG
@@ -96,15 +98,20 @@ def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, port))
         server_socket.listen()
+        server_socket.settimeout(0.1)
         print(f"Server listening on {HOST}:{port}")
         (t := threading.Thread(name="ask_for_commands",target=ask_for_commands)).start()
         THREADS.append(t)
-        (t := threading.Thread(name="process_commands",target=process_commands)).start()
+        t = threading.Thread(name="process_commands",target=process_commands).start()
         THREADS.append(t)
         while not SHUTDOWN_FLAG:
-            conn, addr = server_socket.accept()
-            (t := threading.Thread(name="handle_client",target=handle_client, args=(conn, addr))).start()
-            THREADS.append(t)
+            try:
+                conn, addr = server_socket.accept()
+                threading.Thread(name="handle_client",target=handle_client, args=(conn, addr)).start()
+                THREADS.append(t)
+            except socket.timeout:
+                pass
+    print("Exiting start_server")
 
 def ask_for_commands():
     global SHUTDOWN_FLAG, COMMANDS
@@ -114,6 +121,8 @@ def ask_for_commands():
             continue
         if command.lower() == 'exit':
             COMMANDS.put((-1, ''))
+            SHUTDOWN_FLAG = True  # Set the shutdown flag
+            print("Exiting ask_for_commands")
             break
         else:
             COMMANDS.put((next(command_counter), command))
@@ -127,6 +136,7 @@ def monitor_threads():
             file.write("\n")
             file.flush()
             time.sleep(10)  # Wait for 10 seconds before checking again
+    print("Exiting monitor_threads")
 
 def monitor_clients():
     global SHUTDOWN_FLAG
@@ -137,7 +147,7 @@ def monitor_clients():
                 if not SHUTDOWN_FLAG:
                     CLIENTS.remove((client_id, conn, addr, client_queue))
         time.sleep(0.1)
-
+    print("Exiting monitor_clients")
 
 def exit_handler():
     print("Exiting...")
@@ -147,16 +157,13 @@ if __name__ == "__main__":
         directory = 'logs'
         if not os.path.exists(directory):
             os.makedirs(directory)
-        (t := threading.Thread(name="monitor_threads",target=monitor_threads)).start()
-        THREADS.append(t)
+        #(t := threading.Thread(name="monitor_threads",target=monitor_threads)).start()
+        #THREADS.append(t)
         (t := threading.Thread(name="monitor_clients",target=monitor_clients)).start()
         THREADS.append(t)
         start_server()
     except KeyboardInterrupt:
         SHUTDOWN_FLAG = True  # Set the shutdown flag when Ctrl+C is pressed     
         print("Shutting down server...")
-        for t in THREADS:
-            if t.is_alive():
-                t.join(timeout=1)
     finally:
         atexit.register(exit_handler)
